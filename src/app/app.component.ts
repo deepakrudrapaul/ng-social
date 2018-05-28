d39f97e18f0c2fbc9605dd7ca183d688b5a352b5
deep_paul <deepakrudrapaul@gmail.com>
2018-05-28 17:09:02 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nsl-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'nsl';
+}
